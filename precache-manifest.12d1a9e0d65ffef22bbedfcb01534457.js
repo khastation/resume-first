7ed77dfe2bef4f187f66d6e30f1db6b9a7c4a8ea
Khasnis <khasnis@38f9d34f6123.ant.amazon.com>
2020-05-29 10:08:53 -0700

    adding build folder

---
+++ precache-manifest.12d1a9e0d65ffef22bbedfcb01534457.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac3320762cb5f64312bafbc19072aac",
+    "url": "/resume-first/index.html"
+  },
+  {
+    "revision": "daf2b49f36ff8080665a",
+    "url": "/resume-first/static/js/2.093d7c9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume-first/static/js/2.093d7c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "928381f11ef23a499560",
+    "url": "/resume-first/static/js/main.f1880fdd.chunk.js"
+  },
+  {
+    "revision": "6231e74b5243271da250",
+    "url": "/resume-first/static/js/runtime-main.c161a1d9.js"
+  }
+]);
